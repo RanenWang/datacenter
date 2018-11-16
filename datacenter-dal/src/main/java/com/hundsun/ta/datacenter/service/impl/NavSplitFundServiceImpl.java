31d3b5c170ce2eece4a4566daac5d2b0a3f42e3f
wangpeng <wangpeng616455@outlook.com>
2018-11-16 21:51:55 +0800

    dev分支

--- datacenter-dal/src/main/java/com/hundsun/ta/datacenter/service/impl/NavSplitFundServiceImpl.java
+++ datacenter-dal/src/main/java/com/hundsun/ta/datacenter/service/impl/NavSplitFundServiceImpl.java
@@ -4,30 +4,39 @@
  */
 package com.hundsun.ta.datacenter.service.impl;
 
+import com.hundsun.ta.datacenter.daointerface.NavSplitFundDOMapper;
+import com.hundsun.ta.datacenter.dataobject.NavSplitFundDO;
+import com.hundsun.ta.datacenter.service.NavSplitFundService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.hundsun.ta.datacenter.daointerface.SystemParameterDOMapper;
-import com.hundsun.ta.datacenter.dataobject.SystemParameterDO;
-import com.hundsun.ta.datacenter.service.SystemParameterService;
+import java.util.List;
 
 /**
  * @author wangpeng17355
  * @version $Id: SystemParameterService, v0.1 2018年11月15日 5:26 PM wangpeng17355 Exp $
  */
-@Service(value = "systemParameterService")
-public class NavSplitFundServiceImpl implements SystemParameterService {
+@Service(value = "navSplitFundService")
+public class NavSplitFundServiceImpl implements NavSplitFundService {
 
     @Autowired
-    private SystemParameterDOMapper systemParameterDOMapper;
+    private NavSplitFundDOMapper navSplitFundDOMapper;
 
-    @Override
-    public SystemParameterDO selectByPrimaryKey(int id) {
+    @Override public int insert(NavSplitFundDO navSplitFundDO) {
+       int result = 1;
+       try {
+           result = navSplitFundDOMapper.insert(navSplitFundDO);
+       }catch (Exception e)
+       {
+           throw e;
+       }
+       return result ;
 
-        SystemParameterDO systemParameterDO = new SystemParameterDO();
-        systemParameterDO = systemParameterDOMapper.selectByPrimaryKey(1);
-        return systemParameterDO;
+    }
 
+    @Override public List<NavSplitFundDO> selectAllData() {
+
+      return   navSplitFundDOMapper.selectAllData();
     }
 
 }
